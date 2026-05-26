d2047efe8db28ec96e03c37a7f240db932351ba8
jihanrhmdn <jihanrhmdn24@gmail.com>
2026-05-26 20:45:34 +0800

    jurnal modul 6

---
+++ MODUL-6/challange.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void garis(){
+    cout << "----------------------------------------------------------" << endl;
+}
+
+int main(){
+    cout << "        CYRON TERMINAL: DIVINE SUPPRESSION" << endl;
+    cout << endl;
+    cout << "Xelisa: Exquisite... the synchronization is perfect." << endl;
+    garis ();
+    cout << "1. View Neural Map (Status)" << endl;
+    cout << "2. Inject Neural Thread (Add)" << endl;
+    cout << "3. Purge Corrupted Link (Delete)" << endl;
+    cout << "4. Expand Willpower (Resize)" << endl;
+    cout << "0. Surrender (Exit)" << endl;
+    garis ();
+    cout << "Select Operation:";
+
+    return 0;
+}
